59495201457ecfe45e3380b4dd4467c116fb55dc
Ala Eddine Khiari <135550744+AlaEddine-Khiari@users.noreply.github.com>
2024-03-16 18:21:02 +0100

    Update Test_Script.py

--- Test_Script.py
+++ Test_Script.py
@@ -6,20 +6,22 @@ from Script import connect_to_postgres, insert_cdr, process_cdr_file, get_last_c
 
 class TestScript(unittest.TestCase):
     def setUp(self):
-        # Create a temporary directory and CSV file
+        # Create a temporary directory
         self.temp_dir = tempfile.TemporaryDirectory()
+
+        # Create a temporary CSV file
         self.temp_csv_file = os.path.join(self.temp_dir.name, 'test.csv')
         with open(self.temp_csv_file, 'w') as file:
             file.write("""2024-03-15 10:00:00,1234567890,0987654321,completed,60,recording1.wav
-2024-03-15 10:05:00,0987654321,1234567890,completed,45,recording2
+2024-03-15 10:05:00,0987654321,1234567890,completed,45,recording2.wav
 """)
 
         # Create two mock recording files
-        recording1_path = os.path.join(self.temp_dir.name, 'recording1.wav')
-        recording2_path = os.path.join(self.temp_dir.name, 'recording2.wav')
-        with open(recording1_path, 'wb') as file:
+        self.recording1_path = os.path.join(self.temp_dir.name, 'recording1.wav')
+        self.recording2_path = os.path.join(self.temp_dir.name, 'recording2.wav')
+        with open(self.recording1_path, 'wb') as file:
             file.write(b'Mock recording 1 data')
-        with open(recording2_path, 'wb') as file:
+        with open(self.recording2_path, 'wb') as file:
             file.write(b'Mock recording 2 data')
 
     def tearDown(self):
