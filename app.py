e01eef49bd4ff02f3478c25e4ab8520e729ee845
Ala Eddine Khiari <135550744+AlaEddine-Khiari@users.noreply.github.com>
2024-03-19 00:06:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -60,20 +60,44 @@ def process_cdr_file(file_path):
                     status = "Unvailable"
                     call_recording_data = None
                     duration = "00:00"  # Set duration to 00:00 if billsec equals duration
+def process_cdr_file(file_path):
+    conn = connect_to_postgres()
+    if conn is not None:
+        cursor = conn.cursor()
+        last_one = None
+        last_call_start_date = None
+        with open(file_path, 'r') as csvfile:
+            cdr_reader = csv.reader(csvfile)
+            for row in cdr_reader:
+                if not row:  # Check if the row is empty
+                    continue  # Skip this iteration if the row is empty
+                # Assuming the structure of CSV file: timestamp, source, destination, status, billsec, duration, recording_file_name
+                timestamp, source, destination, status, billsec, duration, recording_file_name = row
+                # Construct the full file path
+                recording_file_path = os.path.join('/ext/recordings', recording_file_name+'.wav')
+                
+                if status == "BUSY":
+                    call_recording_data = None
+                # Check if billsec is equal to duration
+                elif int(duration) == 0:
+                    status = "Unvailable"
+                    call_recording_data = None
+                    duration = "00:00"  # Set duration to 00:00 if billsec equals duration
                 else:
-                    last_call_start_date = get_last_call_start_date(conn, cursor)
-                    call_start_date = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
-                    # Check if call_start date is after the last recorded call_start date
-                    if (recording_file_name != last_one) and (last_call_start_date is None or call_start_date > last_call_start_date):
                     # Convert duration to minutes and seconds format
                     duration = str(timedelta(seconds=int(duration)))
-                    call_recording_data = read_binary_data(recording_file_path)
-                    # Insert Data Into Data Base
-                    cdr_data = (timestamp, source, destination, status, duration, call_recording_data)
-                    insert_cdr(conn, cursor, cdr_data)
-                    last_one = recording_file_name
-                    # Delete the call recording file
-                    os.remove(recording_file_path)
+                    
+                    # Check if call_start date is after the last recorded call_start date
+                    last_call_start_date = get_last_call_start_date(conn, cursor)
+                    call_start_date = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
+                    if (recording_file_name != last_one) and (last_call_start_date is None or call_start_date > last_call_start_date):
+                        call_recording_data = read_binary_data(recording_file_path)
+                        # Insert Data Into Data Base
+                        cdr_data = (timestamp, source, destination, status, duration, call_recording_data)
+                        insert_cdr(conn, cursor, cdr_data)
+                        last_one = recording_file_name
+                        # Delete the call recording file
+                        os.remove(recording_file_path)
         conn.close()
 
 # Function to get the last recorded call_start date from PostgreSQL
