81154a5245d015ba0125cda2e7e0fbed470882b1
Ala Eddine Khiari <135550744+AlaEddine-Khiari@users.noreply.github.com>
2024-03-22 03:36:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import csv
 import psycopg2
 import os
 import time
-from datetime import datetime, timedelta
+from datetime import timedelta
 
 app = Flask(__name__)
 
@@ -42,28 +42,44 @@ def process_cdr_file(file_path):
     if conn is not None:
         cursor = conn.cursor()
         last_one = None
-        last_call_start_date = get_last_call_start_date(conn, cursor)
         with open(file_path, 'r') as csvfile:
             cdr_reader = csv.reader(csvfile)
             for row in cdr_reader:
                 if not row:  # Check if the row is empty
-                    continue  # Skip this iteration if the row is empty
-                # Assuming the structure of CSV file: timestamp, source, destination, status, billsec, duration, recording_file_name
-                timestamp, source, destination, status, billsec, duration, recording_file_name = row
+                    continue  # Skip
+                # Assuming the structure of CSV file: timestamp, source, destination, status, billsec, duration, recording_file_name, aux: for Knowing The person Who answer For Incoming
+                timestamp, source, destination, status, billsec, duration, recording_file_name, aux = row
+                # Delet the row
+                rows.remove(row)
                 # Construct the full file path
                 recording_file_path = os.path.join('/ext/recordings', recording_file_name+'.wav')
-                
-                # Check if billsec is equal to duration
+                # Convert duration to minutes and seconds format
+                duration = str(timedelta(seconds=int(duration)))    
+                     
+                # Check if duration is equal to 0 so the destination is unvailable
                 if (int(duration) == 0) and (status == "ANSWERED"):
-                    status = "Unvailable"
+                    status = "UNVAILABLE"
                     call_recording_data = None
-                    duration = "00:00"  # Set duration to 00:00 if billsec equals duration
-                else:
-                    call_start_date = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
-                    # Check if call_start date is after the last recorded call_start date
-                    if (recording_file_name != last_one) and (last_call_start_date is None or call_start_date >= last_call_start_date):
-                        # Convert duration to minutes and seconds format
-                        duration = str(timedelta(seconds=int(duration)))
+                    cdr_data = (timestamp, source, destination, status, duration, call_recording_data)
+                    insert_cdr(conn, cursor, cdr_data)
+                
+                elif not (destination.isdigit()):
+                    if (status == "ANSWERED"):
+                        call_recording_data = read_binary_data(recording_file_path) 
+                        destination= aux.split("/")[1][:3]                         
+                        cdr_data = (timestamp, source, destination, status, duration, call_recording_data)
+                        insert_cdr(conn, cursor, cdr_data)
+                        os.remove(recording_file_path)
+                                    
+                    elif ((recording_file_name != next(cdr_reader)[6]) and (status != "ANSWERED")) or (not aux):
+                        call_recording_data = None
+                        status = "NO ANSWER"
+                        destination = "No One"
+                        cdr_data = (timestamp, source, destination, status, duration, call_recording_data)
+                        insert_cdr(conn, cursor, cdr_data)
+                        os.remove(recording_file_path)
+                else:                   
+                    if (recording_file_name != last_one):
                         if status == "BUSY":
                             call_recording_data = None
                         else:
@@ -76,11 +92,6 @@ def process_cdr_file(file_path):
                         os.remove(recording_file_path)
         conn.close()
 
-# Function to get the last recorded call_start date from PostgreSQL
-def get_last_call_start_date(conn, cursor):
-    cursor.execute("SELECT MAX(timestamp) FROM cdr_log")
-    last_call_start_date = cursor.fetchone()[0]
-    return last_call_start_date
 
 # Function to read binary data from a file
 def read_binary_data(file_path):
